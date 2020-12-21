8b9805e65595aa82c6984420567310ebd0b83823
Ian Pierce <ianmichaelp@outlook.com>
2020-12-21 09:59:01 -0500

    Adds Best Friend Maker

---
+++ bestfriend.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+print('+---------------------+')
+print('|  Best Friend Maker  |')
+print('|    By Ian Pierce    |')
+print('+---------------------+')
+print()
+
+print("Hi. I'm the computer.")
+name = input("What's your name? ")
+print(f"Hi, {name}! What's your favorite color?")
+favorite_color = input("What's your favorite color? ")
+print(f"No way! My favorite color is also {favorite_color}!")
+favorite_movie = input("What's your favorite movie? ")
+print(f"What!?! My favorite movie is also {favorite_movie}!")
+favorite_game = input("What's your favorite game? ")
+print(f"Get out of here! My favorite game is {favorite_game}, too!")
+print("We like the same things! We should be best friends!")
